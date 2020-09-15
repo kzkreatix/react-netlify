7b3fb870286fb0a7074b9bccf40b3ecf0cb2394c
khizerdev <46632721+khizerdev@users.noreply.github.com>
2020-09-15 17:31:21 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <div className="container-fluid">
         <div className="row">
           <div className="col-md-6">
-          <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style={{border:'none',overflow:'hidden'}} scrolling="no" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+          Hello World
           </div>
           <div className="col-md-6">
 
